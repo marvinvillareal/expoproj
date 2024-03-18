fcb2f8bd48f714c4f8698e7f1090c3fb4a9b60dc
marvin.villareal <babyboymarvin@gmail.com>
2024-03-18 22:09:11 +0800

    added projects

---
+++ todo-js/todosSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let nextTodoId = 0;
+
+const todosSlice = createSlice({
+    name: 'todos',
+    initialState:[{ id: nextTodoId++, text: "task 1", completed: false }],
+    reducers: {
+        addTodo(state, action) {
+            state.push({ id: nextTodoId++, text: action.payload, completed: false })
+        },
+        toggleTodo(state, action) {
+            const todo = state.find(todo => todo.id === action.payload)
+            if (todo) {
+              todo.completed = !todo.completed
+            }
+      
+        }
+    }
+})
+
+export const { addTodo, toggleTodo } = todosSlice.actions
+
+export default todosSlice.reducer
